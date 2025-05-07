d92b97c4fa73e7c5506f6f31c3cb49804cc1d52f
AJGHZ <aldogarciahernandez@hotmail.com>
2025-05-07 02:26:36 -0600

    Modificando archivo de pruebas para el compilador

--- test.cs
+++ test.cs
@@ -7,5 +7,48 @@ class test
         Console.WriteLine("Hello, World!");
         suma(5, 6);
         result(suma);
+
+        if (true)
+        {
+            Console.WriteLine("This is true");
+        }
+        else
+        {
+            Console.WriteLine("This is false");
+        }
+
+        while (true)
+        {
+            Console.WriteLine("This is a while loop");
+            break; // Prevent infinite loop for demonstration purposes
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("This is a for loop: " + i);
+        }
+
+        foreach (var item in new[] { 1, 2, 3 })
+        {
+            Console.WriteLine("This is a foreach loop: " + item);
+        }
+
+        switch (5)
+        {
+            case 1:
+                Console.WriteLine("Case 1");
+                break;
+            case 2:
+                Console.WriteLine("Case 2");
+                break;
+            default:
+                Console.WriteLine("Default case");
+                break;
+        }
+
+        do
+        {
+            Console.WriteLine("This is a do-while loop");
+        } while (false); // Prevent infinite loop for demonstration purposes
     }
 }
